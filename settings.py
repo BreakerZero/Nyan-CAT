09fb95eed92ef41906ae6263397977bacec157ce
Breaker <68560793+BreakerZero@users.noreply.github.com>
2024-11-17 14:45:57 +0100

    fix redis connection ?

--- settings.py
+++ settings.py
@@ -69,7 +69,7 @@ celery.conf.update(
     },
     timezone='UTC'
 )
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
 start_lock = threading.Lock()
 server_started = False
 login_manager = LoginManager()
